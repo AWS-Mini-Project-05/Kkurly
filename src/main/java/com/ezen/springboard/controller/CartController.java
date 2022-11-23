9a5a1de5866fd58b5352865746f2d1f27bf0833c
Haein-Han <godls1131@gmail.com>
2022-11-23 17:39:48 +0900

    1123 메인 헤더 jsp 개발

--- src/main/java/com/ezen/springboard/controller/CartController.java
+++ src/main/java/com/ezen/springboard/controller/CartController.java
@@ -10,13 +10,13 @@ import com.ezen.springboard.service.cart.CartService;
 @Controller
 @RequestMapping("/cart") // 모든 주소 앞에 /user 붙음! (폴더구조라고 생각하면 편함)
 public class CartController {
-	@Autowired
-	private CartService cartService;
-
-	@GetMapping("/cart.do")
-	public String cartView() {
-		
-		return "/cart/cart";
-	}
+//	@Autowired
+//	private CartService cartService;
+//
+//	@GetMapping("/cart.do")
+//	public String cartView() {
+//		
+//		return "/cart/cart";
+//	}
 
 }
